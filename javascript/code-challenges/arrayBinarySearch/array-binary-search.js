e64603249646e7d0262a224ceb46656c5524159d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-01 16:42:50 -0800

    added code to account for expected failure and edge case

--- javascript/code-challenges/arrayBinarySearch/array-binary-search.js
+++ javascript/code-challenges/arrayBinarySearch/array-binary-search.js
@@ -8,13 +8,24 @@ const binarySearch = (array, value) => {
   while (rangeStart <= rangeEnd) {
     let mid = Math.floor((rangeStart + rangeEnd)/2);
 
+    if (array.length === 0) {
+      return -1;
+    }
+
+    if (array.length === 1) {
+      if (array[0] === value) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
     if (array[mid] === value) {
       // array element exists
       return mid;
     } else if (array[mid] < value) {
-      rangeEnd = mid;
+      rangeStart = mid + 1;
     } else if (array[mid] > value) {
-      rangeStart = mid;
+      rangeEnd = mid - 1;
     }
   }
   // array element not found
