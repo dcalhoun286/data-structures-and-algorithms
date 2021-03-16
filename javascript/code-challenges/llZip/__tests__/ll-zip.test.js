14f156c0a00ed3e2ea0949e093cfe0b494644ad9
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-15 20:42:14 -0700

    wrote tests for code challenge

---
+++ javascript/code-challenges/llZip/__tests__/ll-zip.test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const LinkedList = require('../lib/linked-list.js');
+
+const llZip = require('../lib/ll-zip.js');
+
+describe('====== Linked List Class ======', () => {
+  it('should create an empty list on instantiation', () => {
+    let list = new LinkedList();
+    expect(list.head).toEqual(null);
+  });
+
+  it('should zip two linked lists together, alternating nodes between each list', () => {
+    let list1 = new LinkedList();
+    let list2 = new LinkedList();
+
+    let one = 1;
+    let two = 2;
+    let three = 3;
+    let four = 4;
+    let five = 5;
+    let six = 6;
+    let seven = 7;
+    let eight = 8;
+    let nine = 9;
+    let ten = 10;
+
+    list1.insert(nine);
+    list1.insert(seven);
+    list1.insert(five);
+    list1.insert(three);
+    list1.insert(one);
+
+    list2.insert(ten);
+    list2.insert(eight);
+    list2.insert(six);
+    list2.insert(four);
+    list2.insert(two);
+
+    const zippedList = {
+      head: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: {
+              value: 4,
+              next: {
+                value: 5,
+                next: {
+                  value: 6,
+                  next: {
+                    value: 7,
+                    next: {
+                      value: 8,
+                      next: {
+                        value: 9,
+                        next: {
+                          value: 10,
+                          next: null
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+
+    expect(llZip(list1, list2)).toEqual(zippedList);
+  });
+
+});
