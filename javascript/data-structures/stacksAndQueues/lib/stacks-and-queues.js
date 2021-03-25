ddad5597645d0d901f79e926f7305029588f4d54
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-24 23:33:50 -0700

    enqueue(item) passes

--- javascript/data-structures/stacksAndQueues/lib/stacks-and-queues.js
+++ javascript/data-structures/stacksAndQueues/lib/stacks-and-queues.js
@@ -9,6 +9,21 @@ class Queue {
     this.rear = null;
     this.storage = [];
   }
+
+  enqueue(item) {
+
+    if (this.storage.length === 0) {
+      this.storage.push(item);
+      this.front = item;
+      this.rear = item;
+    } else if (this.storage.length >= 1) {
+      this.storage.push(item);
+      this.storage[this.storage.length - 1].next = item;
+      this.rear = item;
+    }
+
+    console.log(this.storage[0]);
+  }
 }
 
 // last in, first out structure
