adb2343172c7e5d24be246e8a39a7c93d85a0416
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-08-30 19:27:49 -0700

    initial commit

---
+++ javascript/code-challenges/treeIntersection/lib/binary-search-tree.js
@@ -0,0 +1,54 @@
+const BinaryTree = require('./binary-tree.js');
+const Node = require('./node.js');
+
+class BinarySearchTree extends BinaryTree {
+  constructor(value) {
+    super(value);
+  }
+
+  add(value) {
+    if (this.contains(value)) { return 'input value cannot already exist in tree'; }
+    let node = new Node(value);
+
+    if (!this.root) { this.root = node; }
+    else {
+      let current = this.root;
+
+      while (current.left || current.right) {
+        if (value < current.value && !current.left) {
+          current.left = node;
+          return;
+        }
+
+        if (value > current.value && !current.right) {
+          current.right = node;
+          return;
+        }
+
+        current = value < current.value ? current.left : current.right;
+      }
+
+      current.value > value ? current.left = node : current.right = node;
+    }
+  }
+
+  contains(value) {
+    if (!this.root) { return false; }
+
+    let current = this.root;
+    let result = false;
+
+    while (current) {
+      if (value === current.value) {
+        result = true;
+        break;
+      }
+
+      current = value < current.value ? current.left : current.right;
+    }
+
+    return result;
+  }
+}
+
+module.exports = BinarySearchTree;
---
+++ javascript/code-challenges/treeIntersection/lib/binary-tree.js
@@ -0,0 +1,48 @@
+const Node = require('./node.js');
+
+class BinaryTree {
+  constructor (value) {
+    this.root = value ? new Node(value) : null;
+  }
+
+  preOrder() {
+    if (!this.root) { return 'Empty tree'; }
+
+    let _walk = (node, values=[]) => {
+      values.push(node.value);
+      if (node.left) { _walk(node.left, values); }
+      if (node.right) { _walk(node.right, values); }
+      return values;
+    };
+
+    return _walk(this.root);
+  }
+
+  inOrder() {
+    if (!this.root) { return 'Empty tree'; }
+
+    let _walk = (node, values=[]) => {
+      if (node.left) { _walk(node.left, values); }
+      values.push(node.value);
+      if (node.right) { _walk(node.right, values); }
+      return values;
+    };
+
+    return _walk(this.root);
+  }
+
+  postOrder() {
+    if (!this.root) { return 'Empty tree'; }
+
+    let _walk = (node, values=[]) => {
+      if (node.left) { _walk(node.left, values); }
+      if (node.right) { _walk(node.right, values); }
+      values.push(node.value);
+      return values;
+    };
+
+    return _walk(this.root);
+  }
+}
+
+module.exports = BinaryTree;
