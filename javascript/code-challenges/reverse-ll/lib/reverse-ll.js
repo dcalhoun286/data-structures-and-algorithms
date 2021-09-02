872a9c8f67812aedf7d6c2c5761d4aba4d81b812
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-09-02 15:25:19 -0700

    deleted corpse code

--- javascript/code-challenges/reverse-ll/lib/reverse-ll.js
+++ javascript/code-challenges/reverse-ll/lib/reverse-ll.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const LinkedList = require ('../../../data-structures/linkedList/lib/linked-list.js');
-
 function reverse(ll) {
 
   if (ll.head) {
@@ -20,12 +18,5 @@ function reverse(ll) {
     ll.head = current;
   }
 }
-
-let list1 = new LinkedList();
-list1.append(2);
-list1.append(4);
-list1.append(6);
-list1.append(8);
-list1.append(10);
 
 module.exports = reverse;
