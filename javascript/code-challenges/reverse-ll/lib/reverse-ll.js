cf0c0dd96611035b1bf43a1493f4f9aea7702a60
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-09-02 15:28:31 -0700

    wrote code so fn returns something

--- javascript/code-challenges/reverse-ll/lib/reverse-ll.js
+++ javascript/code-challenges/reverse-ll/lib/reverse-ll.js
@@ -2,21 +2,22 @@
 
 function reverse(ll) {
 
-  if (ll.head) {
-    let previous = null;
-    let current = ll.head;
-    let next;
+  if (!ll.head) { return 'Empty Linked List'; }
 
-    while (current.next) {
-      next = current.next;
-      current.next = previous;
-      previous = current;
-      current = next;
-    }
+  let previous = null;
+  let current = ll.head;
+  let next;
 
+  while (current.next) {
+    next = current.next;
     current.next = previous;
-    ll.head = current;
+    previous = current;
+    current = next;
   }
+
+  current.next = previous;
+
+  return ll;
 }
 
 module.exports = reverse;
